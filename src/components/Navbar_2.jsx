fc5b72b2d0973cc1e75a01cb7ef7d9b60c5d29d8
Subho808 <subhojitsaha808@gmail.com>
2025-03-01 02:38:27 +0530

    navbar correction

--- src/components/Navbar_2.jsx
+++ src/components/Navbar_2.jsx
@@ -20,103 +20,107 @@ export default function Navbar() {
 
   return (
     <div
-      className={`flex items-center justify-center py-8 px-6 md:px-16 fixed z-50 w-screen transition-all duration-300 ${
+      className={`flex items-center justify-center py-8 px-8 fixed z-50 w-screen transition-all duration-300 ${
         isScrolled
           ? "bg-gradient-to-b from-white to-transparent"
           : "bg-transparent"
       }`}
     >
       <div className="flex items-center justify-between w-full">
-        {/* LOGO - Always on the left */}
-        <a href="/">
-          <img src={rkrGrp} className="h-8 md:h-10 lg:h-12" alt="logo" />
-        </a>
-
-        {/* HAMBURGER MENU (Only in mobile view) */}
-        <section className="lg:hidden">
-          <div
-            className="HAMBURGER-ICON space-y-2 ml-auto"
-            onClick={() => setIsNavOpen((prev) => !prev)}
-          >
-            <span className="block h-0.5 w-8 bg-gray-600"></span>
-            <span className="block h-0.5 w-8 bg-gray-600"></span>
-            <span className="block h-0.5 w-8 bg-gray-600"></span>
-          </div>
-        </section>
-
-        {/* MOBILE MENU OVERLAY */}
-        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-          <div
-            className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-            onClick={() => setIsNavOpen(false)}
-          >
-            <svg
-              className="h-8 w-8 text-gray-600"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
-          </div>
-          <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] font-semibold">
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/">Home</a>
-            </li>
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/whyUs">Why us?</a>
-            </li>
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/OurServices">Our Services</a>
-            </li>
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/FutureBusiness">Future Business</a>
-            </li>
-          </ul>
+        <div>
+          {/* LOGO */}
+          <a href="/">
+            <img
+              src={rkrGrp}
+              className="h-8 md:h-10 lg:h-12 text-left"
+              alt="logo"
+            />
+          </a>
         </div>
 
-        {/* DESKTOP MENU */}
-        <ul className="DESKTOP-MENU hidden space-x-12 font-semibold lg:flex">
-          <li>
-            <a
-              href="/"
-              className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
+        <nav>
+          <section className="MOBILE-MENU flex lg:hidden">
+            <div
+              className="HAMBURGER-ICON space-y-2"
+              onClick={() => setIsNavOpen((prev) => !prev)}
             >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="/about"
-              className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="/OurServices"
-              className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
-            >
-              Our Services
-            </a>
-          </li>
-          <li>
-            <a
-              href="/FutureBusiness"
-              className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
-            >
-              Future Business
-            </a>
-          </li>
-        </ul>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            </div>
+
+            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+              <div
+                className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <svg
+                  className="h-8 w-8 text-gray-600"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] font-semibold">
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/">Home</a>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/whyUs">Why us?</a>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/OurServices">Our Services</a>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/FutureBusiness">Future Business</a>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <ul className="DESKTOP-MENU hidden space-x-12 font-semibold lg:flex">
+            <li>
+              <a
+                href="/"
+                className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/about"
+                className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="/OurServices"
+                className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
+              >
+                Our Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="/FutureBusiness"
+                className="text-white transition-all duration-300 hover:text-yellow-400 hover:underline hover:font-bold"
+              >
+                Future Business
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      {/* INLINE STYLES FOR MENU TOGGLE */}
       <style>
         {`
           .hideMenuNav {
