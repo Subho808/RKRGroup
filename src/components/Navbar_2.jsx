744efa1eca120be21c4bfd03d450b42229195199
Subho808 <subhojitsaha808@gmail.com>
2025-03-01 10:26:18 +0530

    navbar change a tag to link tag 1

--- src/components/Navbar_2.jsx
+++ src/components/Navbar_2.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import rkrGrp from "../assets/image-removebg-preview.png";
 
 import "../index.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
